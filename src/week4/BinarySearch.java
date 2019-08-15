e407c4e36a3ae6e2ee98309f101218c1263e6d18
Chujie Chen <cchen641@gatech.edu>
2019-08-14 22:17:29 -0400

    week4 - 1/6 -v2

--- src/week4/BinarySearch.java
+++ src/week4/BinarySearch.java
@@ -6,24 +6,44 @@ import java.util.*;
 public class BinarySearch {
 
     static int binarySearch(int[] a, int x) {
-        int left = 0, right = a.length;
-        //write your code here
-        int mid = left + (right - left) / 2;
-        if(x == a[mid]) {
-        	return mid;
-        }
-        else if(x < a[mid]) {
-        	if(left >= mid)
-        		return -1;
-        	int[] newA = Arrays.copyOfRange(a, left, mid);
-        	return binarySearch(newA, x);
-        }
-        else {
-        	if(mid + 1 >= right)
-        		return -1;
-			int[] newA = Arrays.copyOfRange(a, mid + 1, right);
-			return binarySearch(newA, x);
-		}
+    	/*
+    	 *  approach 1: recursive call - divide and conquer
+    	 */
+//        int left = 0, right = a.length;
+//        //write your code here
+//        int mid = left + (right - left) / 2;
+//        if(x == a[mid]) {
+//        	return mid;
+//        }
+//        else if(x < a[mid]) {
+//        	if(left >= mid)
+//        		return -1;
+//        	int[] newA = Arrays.copyOfRange(a, left, mid);
+//        	return binarySearch(newA, x);
+//        }
+//        else {
+//        	if(mid + 1 >= right)
+//        		return -1;
+//			int[] newA = Arrays.copyOfRange(a, mid + 1, right);
+//			return binarySearch(newA, x);
+//		}
+    	/*
+    	 * approach 2: divide and divide 
+    	 */
+    	int left = 0, right = a.length - 1;
+    	while(left <= right) {
+    		int mid = left + (right - left)/ 2;
+    		if(a[mid] == x)
+    			return mid;
+    		else if(x < mid){
+    			right = mid - 1;				
+			}
+    		else {
+				left = mid + 1;
+			}
+    	}
+    	return -1;
+    	
     }
 
     static int linearSearch(int[] a, int x) {
