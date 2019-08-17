c2fc09cdb9ca069ca2220557ea2d485965a1e66c
Chujie Chen <cchen641@gatech.edu>
2019-08-17 00:51:23 -0400

    week4 - 3/6 - v3

--- src/week4/Sorting.java
+++ src/week4/Sorting.java
@@ -16,42 +16,106 @@ public class Sorting {
     	 * then smaller than
     	 */
     	// get no-greater-than region
-    	int x = a[l];
-//    	System.out.println(x);
-    	// x should be the one
-    	for (int i = l + 1; i <= r; i++) {
-    		if(a[i] <= x) {
-    			m1++;
-    			int t = a[i];
-    			a[i] = a[m1]; 
-    			a[m1] = t; 
-    		}
-    	}
-    	int t = a[l];
-        a[l] = a[m1];
-        a[m1] = t;
-        
-        m2 = m1;
-//        System.out.println(x);
-    	// divide this region into two
-    	for(int i = m2 - 1; i >= l; i--) {
-    		if(a[i] >= x) {
-    			m1--;
-    			t = a[i];
-    			a[i] = a[m1]; 
-    			a[m1] = t; 
-    		}
-    	}
-    	t = a[m2];
-        a[m2] = a[m1];
-        a[m1] = t;
-        
-    	int[] m = {m1, m2};
-//    	System.out.println(m1+"and"+ m2);
-    	return m;
+//    	int x = a[l];
+//    	// x should be the one
+//    	for (int i = l + 1; i <= r; i++) {
+//    		if(a[i] <= x) {
+//    			m1++;
+//    			int t = a[i];
+//    			a[i] = a[m1]; 
+//    			a[m1] = t; 
+//    		}
+//    	}
+//    	int t = a[l];
+//        a[l] = a[m1];
+//        a[m1] = t;
+//        
+//        m2 = m1;
+//    	// divide this region into two
+//    	for(int i = m2 - 1; i >= l; i--) {
+//    		if(a[i] >= x) {
+//    			m1--;
+//    			t = a[i];
+//    			a[i] = a[m1]; 
+//    			a[m1] = t; 
+//    		}
+//    	}
+//    	t = a[m2];
+//        a[m2] = a[m1];
+//        a[m1] = t;
+//        
+//    	int[] m = {m1, m2};
+//    	// for test
+//        for(int i=0; i<a.length;i++) {
+//        	System.out.print(a[i]);
+//        	System.out.print(" ");
+//        }
+//        ///////////
+//    	return m;
     	/*
     	 * approach 2
+    	 * from left to right: smaller
+    	 * from right to left: greater
     	 */
+//    	int x = a[l];
+//    	// x should be the one
+//    	for (int i = l + 1; i <= r; i++) {
+//    		if(a[i] < x) {
+//    			m1++;
+//    			int t = a[i];
+//    			a[i] = a[m1]; 
+//    			a[m1] = t; 
+//    		}
+//    	}
+//    	int t = a[l];
+//        a[l] = a[m1];
+//        a[m1] = t;
+//        
+//        
+//        for (int j = r; j >= l; j--) {
+//    		if(a[j] > x) {
+//    			t = a[j];
+//    			a[j] = a[m2]; 
+//    			a[m2] = t; 
+//    			m2--;
+//    		}
+//    	}
+//        int[] m = {m1, m2};
+//	     // for test
+////        for(int i=0; i<a.length;i++) {
+////	      	System.out.print(a[i]);
+////	      	System.out.print(" ");
+////	    }
+//        return m;
+        
+        /*
+         * approach 3: single scan
+         * https://github.com/mablatnik/Algorithmic-Toolbox/blob/master/algorithmic_toolbox/week_4/03_divide_and_conquer_starter_files_20160804/sorting/Sorting.java
+         */
+    	int x = a[l];
+        for(int i=l; i<=m2; i++) {  // starts from l instead of 0 !!!
+//        	System.out.println(i);
+        	if(a[i] < x) {
+        		int temp = a[i];
+        		a[i] = a[m1];
+        		a[m1] = temp; 
+        		m1++;
+        	}
+        	while(a[i] > x && i <= m2) { // the second condition is important
+        		int temp = a[i];
+        		a[i] = a[m2];
+        		a[m2] = temp; 
+        		m2--;
+			}
+        }
+        int[] m = {m1, m2};
+        // for test
+//	    for(int i=0; i<a.length;i++) {
+//	      	System.out.print(a[i]);
+//	      	System.out.print(" ");
+//	    }
+	    ////////
+	    return m;
     }
 
     private static int partition2(int[] a, int l, int r) {
@@ -99,6 +163,12 @@ public class Sorting {
         for (int i = 0; i < n; i++) {
             System.out.print(a[i] + " ");
         }
+        // test partition3
+//        int[] a = {6,2,5,3,6,8,7};
+//        int[] m = partition3(a, 0, a.length - 1);
+//        System.out.println(" ");
+//        System.out.println(m[0]);
+//        System.out.println(m[1]);
     }
 
     static class FastScanner {
